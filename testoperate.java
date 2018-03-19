dceb382dfa36e7b3627398aff8986e97b550e5b1
Sirapong Phoosawan <34246371+tongsmartza@users.noreply.github.com>
2018-03-19 23:18:21 +0700

    add function

--- testoperate.java
+++ testoperate.java
@@ -15,26 +15,32 @@ public class testoperate {
 	}
 
 	@Test
-	public void test() {
+	public void testadd() {
 		
 		assertEquals(10,opper.adder(5, 5),0.001);
 		assertEquals(5,opper.adder(5, 0),0.001);
 		assertEquals(5,opper.adder(0, 5),0.001);
 		assertEquals(-10,opper.adder(-5, -5),0.001);
 		assertEquals(5,opper.adder(10, -5),0.001);
-		
+	}
+	
+	public void testsub() {
 		assertEquals(5,opper.subtracter(10, 5),0.001);
 		assertEquals(5,opper.subtracter(5, 0),0.001);
 		assertEquals(-5,opper.subtracter(0, 5),0.001);
 		assertEquals(0,opper.subtracter(-5, -5),0.001);
 		assertEquals(10,opper.subtracter(5, -5),0.001);
-		
+	}
+	
+	public void testmul() {
 		assertEquals(18,opper.multiplier(9, 2),0.001);
 		assertEquals(0,opper.multiplier(5, 0),0.001);
 		assertEquals(0,opper.multiplier(0, 5),0.001);
 		assertEquals(-18,opper.multiplier(9, -2),0.001);
 		assertEquals(-18,opper.multiplier(-2, 9),0.001);
-		
+	}
+	
+	public void testdivide() {
 		assertEquals(5,opper.divider(20, 4),0.001);
 		assertEquals(5,opper.divider(5, 1),0.001);
 		assertEquals(5,opper.divider(1, 5),0.001);
